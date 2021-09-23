2da67a7132b8f4cf5841627e6bf722ee9f137fdc
root <danfmihai@hotmail.com>
2021-09-23 10:19:04 -0400

    initial commit

---
+++ instance.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "example" {
+    instance_type = "t2.micro"
+    ami = "ami-087c17d1fe0178315"
+}
---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+      bucket = "september-terraform-2"
+      key = "terraform2"
+      region = "us-east-1"
+  }
+}
