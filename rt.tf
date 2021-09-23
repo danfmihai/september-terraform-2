fb3340aee314f9e3880e2e8c6dcf42dabf0bb010
root <danfmihai@hotmail.com>
2021-09-23 11:11:49 -0400

    initial commit

---
+++ rt.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "dev" {
+    vpc_id = aws_vpc.dev.id 
+
+    route = [ 
+        {
+      cidr_block = "10.0.1.0/24"
+      gateway_id = aws_internet_gateway.dev.id
+    }
+     ]
+}
