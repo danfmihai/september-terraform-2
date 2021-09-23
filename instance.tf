4f3bdd37fc474383133dbf1b73d4e785f9b59857
root <danfmihai@hotmail.com>
2021-09-23 10:48:11 -0400

    added resources

--- instance.tf
+++ instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "example" {
     instance_type = var.instance_type
     ami = var.ami
+    key_name = var.key_name
 }
--- variable.tf
+++ variable.tf
@@ -2,5 +2,11 @@ variable "instance_type" {
     default = "t2.micro"  
 }
 variable "ami" {
+  description = "AMI deafult Amazon Image" 
   default = "ami-087c17d1fe0178315"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"   
+  default = "terraform_admin"
 }
